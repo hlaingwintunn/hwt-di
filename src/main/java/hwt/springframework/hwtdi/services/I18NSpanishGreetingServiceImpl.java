b522255f17dff33d9b12f68ee464af2fa3f3b372
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-07 16:54:07 +0800

    Default Spring Profile

--- src/main/java/hwt/springframework/hwtdi/services/I18NSpanishGreetingServiceImpl.java
+++ src/main/java/hwt/springframework/hwtdi/services/I18NSpanishGreetingServiceImpl.java
@@ -3,7 +3,7 @@ package hwt.springframework.hwtdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("SN")
+@Profile({"SN", "default"})
 @Service("i18nService")
 public class I18NSpanishGreetingServiceImpl implements GreetingService {
     @Override
